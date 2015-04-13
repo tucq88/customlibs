a9f974dc15212d1df89eeb3512507c459dcb1345
hoatle <hoatle@teracy.com>
2015-04-13 12:39:42 +0700

    add jquery.ui.boxer, jquery.monkey-patch
    ref: https://github.com/iorad/iorad/issues/1834

---
+++ jquery.money-patch/jquery.money-patch.js
@@ -0,0 +1,185 @@
+/**
+ * monkey patching the jquery lib
+ */
+
+// from http://stackoverflow.com/questions/10045423/determine-whether-user-clicking-scrollbar-or-content-onclick-for-native-scroll
+(function ($) {
+
+  $(function () {
+    $.fn.hasScroll = function (axis) {
+      var overflow = this.css('overflow'),
+          overflowAxis;
+
+      if (typeof axis == 'undefined' || axis == 'y') overflowAxis = this.css('overflow-y');
+      else overflowAxis = this.css('overflow-x');
+
+      var bShouldScroll = this.get(0).scrollHeight > this.innerHeight();
+
+      var bAllowedScroll = (overflow == 'auto' || overflow == 'visible') ||
+          (overflowAxis == 'auto' || overflowAxis == 'visible');
+
+      var bOverrideScroll = overflow == 'scroll' || overflowAxis == 'scroll';
+
+      return (bShouldScroll && bAllowedScroll) || bOverrideScroll;
+    };
+
+    $.fn.mousedown = function (data, fn) {
+      if (fn == null) {
+        fn = data;
+        data = null;
+      }
+      var o = fn;
+      fn = function (e) {
+        if (!inScrollRange(e)) {
+          return o.apply(this, arguments);
+        }
+        return;
+      };
+      if (arguments.length > 0) {
+        return this.bind('mousedown', data, fn);
+      }
+      return this.trigger('mousedown');
+    };
+
+    $.fn.mouseup = function (data, fn) {
+      if (fn == null) {
+        fn = data;
+        data = null;
+      }
+      var o = fn;
+      fn = function (e) {
+        if (!inScrollRange(e)) {
+          return o.apply(this, arguments);
+        }
+        return;
+      };
+      if (arguments.length > 0) {
+        return this.bind('mouseup', data, fn);
+      }
+      return this.trigger('mouseup');
+    };
+
+    $.fn.mousedownScroll = function (data, fn) {
+      if (fn == null) {
+        fn = data;
+        data = null;
+      }
+      var o = fn;
+      fn = function (e) {
+        if (inScrollRange(e)) {
+          e.type = 'mousedownscroll';
+          return o.apply(this, arguments);
+        }
+        return;
+      };
+      if (arguments.length > 0) {
+        return this.bind('mousedown', data, fn);
+      }
+      return this.trigger('mousedown');
+    };
+
+    $.fn.mouseupScroll = function (data, fn) {
+      if (fn == null) {
+        fn = data;
+        data = null;
+      }
+      var o = fn;
+      fn = function (e) {
+        if (inScrollRange(e)) {
+          e.type = 'mouseupscroll';
+          return o.apply(this, arguments);
+        }
+        return;
+      };
+      if (arguments.length > 0) {
+        return this.bind('mouseup', data, fn);
+      }
+      return this.trigger('mouseup');
+    };
+
+    var RECT = function () {
+      this.top = 0;
+      this.left = 0;
+      this.bottom = 0;
+      this.right = 0;
+    };
+
+    function inRect(rect, x, y) {
+      return (y >= rect.top && y <= rect.bottom) &&
+          (x >= rect.left && x <= rect.right)
+    }
+
+
+    var scrollSize = measureScrollWidth();
+
+    function inScrollRange(event) {
+      var x = event.pageX,
+          y = event.pageY,
+          e = $(event.target),
+          hasY = e.hasScroll(),
+          hasX = e.hasScroll('x'),
+          rX = null,
+          rY = null,
+          bInX = false,
+          bInY = false;
+
+      if (hasY) {
+        rY = new RECT();
+        rY.top = e.offset().top;
+        rY.right = e.offset().left + e.width();
+        rY.bottom = rY.top + e.height();
+        rY.left = rY.right - scrollSize;
+
+        //if(hasX) rY.bottom -= scrollSize;
+        bInY = inRect(rY, x, y);
+      }
+
+      if (hasX) {
+        rX = new RECT();
+        rX.bottom = e.offset().top + e.height();
+        rX.left = e.offset().left;
+        rX.top = rX.bottom - scrollSize;
+        rX.right = rX.left + e.width();
+
+        //if(hasY) rX.right -= scrollSize;
+        bInX = inRect(rX, x, y);
+      }
+
+      return bInX || bInY;
+    }
+
+    $(document).on('mousedown', function (e) {
+      //Determine if has scrollbar(s)
+      if (inScrollRange(e)) {
+        $(e.target).trigger('mousedownScroll');
+      }
+    });
+
+    $(document).on('mouseup', function (e) {
+      if (inScrollRange(e)) {
+        $(e.target).trigger('mouseupScroll');
+      }
+    });
+  });
+
+  function measureScrollWidth() {
+    var scrollBarMeasure = $('<div />');
+    $('body').append(scrollBarMeasure);
+    scrollBarMeasure.width(50).height(50)
+        .css({
+          overflow: 'scroll',
+          visibility: 'hidden',
+          position: 'absolute'
+        });
+
+    var scrollBarMeasureContent = $('<div />').height(1);
+    scrollBarMeasure.append(scrollBarMeasureContent);
+
+    var insideWidth = scrollBarMeasureContent.width();
+    var outsideWitdh = scrollBarMeasure.width();
+    scrollBarMeasure.remove();
+
+    return outsideWitdh - insideWidth;
+  }
+
+})(jQuery);
---
+++ jquery.ui.boxer/jquery.ui.boxer.js
@@ -0,0 +1,104 @@
+/**
+ * jquery.ui.boxer
+ *
+ * @author hoatle
+ * @since Apr 8, 2015
+ *
+ * inspired by http://jsbin.com/azare/988/edit?js,output
+ */
+
+$.widget('ui.boxer', $.ui.mouse, {
+  options: {
+    version: '0.1.0',
+    disabled: true, //disabled by default
+    appendTo: 'body',  //TODO(hoatle): support "parent" and relative positioned parent
+    helperBorder: '1px dotted black',
+    helperClass: '',
+    cursor: 'crosshair'
+  },
+  _init: function() {
+    this.element.addClass('ui-boxer');
+    this.originalCursor = this.element.css('cursor');
+    this.dragged = false;
+    this._mouseInit();
+    this.helper = $(document.createElement('div'))
+        .css({border: this.options.helperBorder})
+        .addClass(this.options.helperClass)
+        .addClass('ui-boxer-helper');
+  },
+  _destroy: function() {
+    this.element
+        .removeClass('ui-boxer ui-boxer-disabled')
+        .removeData('boxer')
+        .unbind('.boxer')
+        .css('cursor', this.originalCursor);
+    this._mouseDestroy();
+    return this;
+  },
+  _setOption: function(key, value) {
+    if (key === 'disabled') {
+      this.element.css('cursor', value ? this.originalCursor: this.options.cursor);
+    }
+    this._super(key, value);
+  },
+  _mouseStart: function(event) {
+
+    this.startedPosition = {
+      x: event.pageX,
+      y: event.pageY
+    };
+
+    if (this.options.disabled) {
+      return
+    }
+
+    var options = this.options;
+
+    this.helper
+        .css({border: this.options.helperBorder})
+        .addClass(this.options.helperClass);
+
+    this._trigger('start', event);
+
+    $(options.appendTo).append(this.helper);
+
+    this.helper.css({
+      'z-index': 100,
+      'position': 'absolute',
+      'left': event.clientX,
+      'top': event.clientY,
+      'width': 0,
+      'height': 0
+    });
+  },
+  _mouseDrag: function(event) {
+    this.dragged = true;
+
+    if (this.options.disabled) {
+      return;
+    }
+
+    var x1 = this.startedPosition['x'], y1 = this.startedPosition['y'], x2 = event.pageX, y2 = event.pageY;
+    if (x1 > x2) { var tmp = x2; x2 = x1; x1 = tmp; }
+    if (y1 > y2) { tmp = y2; y2 = y1; y1 = tmp; }
+    this.helper.css({left: x1, top: y1, width: x2-x1, height: y2-y1});
+
+    this._trigger('drag', event);
+
+    return false;
+  },
+  _mouseStop: function(event) {
+    this.dragged = false;
+    if (this.options.disabled) {
+      return;
+    }
+
+    var cloneBox = this.helper.clone();
+
+    this._trigger('stop', event, cloneBox);
+
+    this.helper.remove();
+
+    return false;
+  }
+});
